3256db0543ebb49d6598667e898444d3d943d69a
Claire Wu <claire.xy@gmail.com>
2020-02-22 17:13:16 +0000

    Initial commit

---
+++ app/src/main/java/com/example/teslaspigeon/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.teslaspigeon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView webView = (WebView) findViewById(R.id.webview_main);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
+        webView.loadUrl("http://www.google.com");
+
+    }
+
+    private class Callback extends WebViewClient {  //HERE IS THE MAIN CHANGE.
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
+    }
+}
